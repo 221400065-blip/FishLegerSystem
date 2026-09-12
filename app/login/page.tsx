eb77b4bbb315af4975275192eed3ac13df517939
221400065-blip <221400065@gift.edu.pk>
2026-09-12 16:18:32 +0500

    Ayesha

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,6 +8,7 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
+  Ayesha
   const router = useRouter();
 
   const handleSignIn = (e: React.FormEvent) => {
